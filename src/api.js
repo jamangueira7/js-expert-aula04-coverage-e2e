bd462a2ff674686fc7727e8a45853cc054d7bf85
João Mangueira <jamjs7@gmail.com>
2021-05-28 17:06:45 -0300

    primeiva versão do teste

---
+++ src/api.js
@@ -0,0 +1,30 @@
+const http = require('http');
+
+const routes = {
+
+    '/contact:get': (request, response) => {
+        response.write('contact us page.');
+        return response.end();
+    },
+
+    default: (request, response) => {
+        response.write('Hello World!');
+        return response.end();
+    }
+};
+
+const header = function (request, response) {
+    const { url, method } = request;
+    const routeKey = `${url}:${method.toLowerCase()}`
+    const chosen = routes[routeKey] || routes.default;
+
+    response.writeHead(200, {
+        'Content-Type': 'text/html'
+    });
+
+    return chosen(request, response);
+};
+
+const app = new http.createServer(header).listen(3000, () => console.log('app running at', 3000));
+
+module.exports = app;
